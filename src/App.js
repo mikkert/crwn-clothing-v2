23042b36c5162ad35e7debd5000667f5d38be3bc
Mikkert <miktenholt@gmail.com>
2022-10-04 23:21:35 +0200

    updating our directory page wirth directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
+import categories from './categories.json'
+import Directory from './components/directory/directory.component'
+
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ];
+   return <Directory categories={categories} />
+}
 
-  return (
-    <div className='categories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
-          <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default App;
+export default App
