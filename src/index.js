302947d7d647006d5644959bd3082557444e241d
Mikkert <miktenholt@gmail.com>
2022-10-17 23:17:48 +0200

    Adding cart icon, cart dropdown and cart context

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ import { ProductsProvider } from './contexts/products.context'
 
 import './index.scss'
 import reportWebVitals from './reportWebVitals'
+import { CartProvider } from './contexts/cart.context'
 
 ReactDOM.render(
    <React.StrictMode>
       <BrowserRouter>
          <UserProvider>
             <ProductsProvider>
-               <App />
+               <CartProvider>
+                  <App />
+               </CartProvider>
             </ProductsProvider>
          </UserProvider>
       </BrowserRouter>
